b73792e0c83ac005c14eaa98d568463c1f05e46b
HenLovelyBird <voxylophone@gmail.com>
2019-10-31 07:44:28 +0100

    someJS

---
+++ moreBootstrap.js
@@ -0,0 +1,15 @@
+//i would like to target the badges and make every other one badge-light and every 2nd one badge-dark
+
+var alternateBadges = document.querySelctor('span .badge')
+span .badge:nth(2n)
+span .badge:nth(3n)
+
+alternateBadges.style.type (badge-light)
+
+
+//EXERCISE 6 Add a DELETE bootstrap button on each row of the tracklist. Clicking on the button the row must disappear
+//add the button onclick event at the bottom of the html page then write a function here for the button 
+
+function removeRow(){
+    var = document.querySelector('')
+}
